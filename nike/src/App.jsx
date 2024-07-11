44e8d0e92577bc09f59b75e7d8a11012d5ae18ab
hafsa chenni <hafsachenni77@gmail.com>
2024-07-11 14:54:57 +0200

    services/products/about us sections done

--- nike/src/App.jsx
+++ nike/src/App.jsx
@@ -18,7 +18,7 @@ const App = () => (
       <Hero />
     </section>
     <section className="padding">
-      PopularProducts
+      <PopularProducts />
     </section>
     <section className="padding">
       <SuperQuality />
